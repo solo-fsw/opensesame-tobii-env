9c8c94a98c473014cec0d0e25bca45aceb8639c2
spruitim <i.m.spruit@fsw.leidenuniv.nl>
2024-05-31 14:00:27 +0200

    updated files for os4

---
+++ opensesame4/run_OpenSesame_in_env.bat
@@ -0,0 +1,23 @@
+@echo off
+
+
+rem Runs OpenSesame in a specific environment.
+
+
+rem Set the environment name:
+set ENVNAME=opensesame4-py310
+
+rem Check for conda:
+call find_conda.bat
+if %CONDAPATH%==0 (
+    echo OpenSesame cannot be launched in %ENVNAME%
+    exit /B
+)
+
+rem Activate the environment, and run OS:
+call %CONDAPATH%condabin\activate.bat %ENVNAME%
+opensesame
+
+
+rem Note, OpenSesame takes in the exp filename as the first arg.
+rem As such, ypass a filename to it to automatically open an experiment.
